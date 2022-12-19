eb6cc98bf006626583d9dffbb875fb02d0bb33e3
jyotikrishnatshelatr <shelarjyoti2506@gmail.com>
2022-12-19 13:45:45 +0530

    basic commit
    hi alll....i have created project on github.comgo ahead clone it.start ur respective task

---
+++ SampleProject/src/pageLayer/TestBase.java
@@ -0,0 +1,10 @@
+package pageLayer;
+
+public class TestBase {
+public static void main(String[]args) {
+	System.out.println("hello world");
+	System.out.println("created testbase");
+	System.out.println("Added depedencies");
+	
+}
+}
